db92e756fdb7c6becadc87673877d5fb419c9852
Moon_minhee <118891828+mooooonmin@users.noreply.github.com>
2025-01-13 13:49:25 +0900

    Time: 781 ms (15.88%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -5,4 +5,5 @@ SELECT
     ROUND(AVG(rating / position), 2) AS quality,
     ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*) * 100, 2) AS poor_query_percentage  
 FROM Queries
+WHERE query_name IS NOT NULL
 GROUP BY query_name
