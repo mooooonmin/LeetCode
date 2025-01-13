9acb512b3ed2f88ed4574e87e7347875abf15ee7
Moon_minhee <118891828+mooooonmin@users.noreply.github.com>
2025-01-13 11:13:04 +0900

    Time: 1148 ms (41.64%), Space: 0B (100%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT
+    P.product_id,
+    CASE WHEN SUM(units) <> 0 THEN ROUND(SUM(price * units) / SUM(units), 2) ELSE 0 END AS average_price
+FROM 
+    Prices P 
+LEFT JOIN 
+    UnitsSold US 
+ON P.product_id = US.product_id
+AND US.purchase_date >= P.start_date
+AND US.purchase_date <= P.end_date
+GROUP BY P.product_id
